7304f34537d002bbb2fefa152bb84172e74350f1
DiVi-SM <darenmaynard531@gmail.com>
2026-09-12 23:18:21 +1000

    Add files via upload

--- Server/node_modules/aggregate-error.js
+++ Server/node_modules/aggregate-error.js
@@ -1,9 +1,2 @@
 'use strict';
-require('../modules/es.error.cause');
-require('../modules/es.aggregate-error');
-require('../modules/es.aggregate-error.cause');
-require('../modules/es.array.iterator');
-require('../modules/es.string.iterator');
-var path = require('../internals/path');
-
-module.exports = path.AggregateError;
+module.exports = require('../full/aggregate-error');
